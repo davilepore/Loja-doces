0095c4828b516782fbf9a19abc9b67ebd176d2a3
davilepore <davilepore218@gmail.com>
2026-02-23 10:32:16 -0300

    feat(ui): add navbar component

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <div className="flex justify-between">
+      <div>alo</div>
+      <div>
+        <h2>Doces</h2>
+        <h2>Mabecky</h2>
+      </div>
+      <div>
+        <h2>sei la</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
