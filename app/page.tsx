0d7cded9fb3c762e618775bb4015c75818ee452b
davilepore <davilepore218@gmail.com>
2026-02-24 19:33:21 -0300

    style: remove navbar from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import Contact from "./_components/Contact";
 import AboutUs from "./_components/AboutUs";
 import HeroCarousel from "./_components/HeroCarousel";
-import Navbar from "./_components/Navbar";
+
 import SweetTypes from "./_components/SweetTypes";
 import Footer from "./_components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-
       <HeroCarousel />
 
       <SweetTypes />
