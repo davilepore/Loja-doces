5fb6b85c7e11536f41f1670b8502a06be3dbe365
davilepore <davilepore218@gmail.com>
2026-02-24 19:34:34 -0300

    chore: add types file

---
+++ lib/types.ts
@@ -0,0 +1,26 @@
+export const categorias = [
+  {
+    id: 1,
+    nome: "bolos",
+    slug: "bolos",
+    titulo: "Nossos Bolos",
+    imagem: "/bolo.jpg",
+    descricao: "Bolos artesanais feitos com carinho.",
+  },
+  {
+    id: 2,
+    nome: "docinhos",
+    slug: "docinhos",
+    titulo: "Nossos Docinhos",
+    imagem: "/brigadeiro.avif",
+    descricao: "Docinhos gourmet para qualquer ocasião.",
+  },
+  {
+    id: 3,
+    nome: "sobremesas",
+    slug: "sobremesas",
+    titulo: "Nossas Sobremesas",
+    imagem: "/brownie.jpg",
+    descricao: "Sobremesas irresistíveis.",
+  },
+];
